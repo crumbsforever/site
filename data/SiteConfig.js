9c84ec62b1f181e4223b9379e5b8a5553d4982cf
crumbs-forever <crumbsforever@gmail.com>
2023-12-01 19:22:49 -0500

    Again

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ const config = {
   siteTitleShort: "Crumbs Forever", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "CrumbsForever", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024x1024.png", // Logo used for SEO and manifest.
-  siteUrl: "", // Domain of your website without pathPrefix.
+  siteUrl: "https://crumbsforever.com", // Domain of your website without pathPrefix.
   pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A GatsbyJS Blog Template for blogging purpose.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
